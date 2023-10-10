b69f03f27eb6e1e301b488e8dae502f9daabfe82
Nika <nikabilanych@gmail.com>
2023-10-10 23:06:54 +0200

    Update page.tsx

---
+++ app/(Routes)/test/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+  return (
+    <div>
+
+      <Button variant="ghost">Click me</Button>
+
+
+    </div>
+    )}
