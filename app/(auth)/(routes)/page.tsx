90f2ac3e09a6b332f9712a705caa026156e04043
Nika <nikabilanych@gmail.com>
2023-10-12 17:35:07 +0200

    completed auth.

---
+++ app/(auth)/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+import { UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <UserButton/>
+    this is a protected route
+
+
+
+    </div>
+    )}
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   
 const AuthLayout = ({ children }:{ children:React.ReactNode}) => {
     return (
-        <div className="bg-red-500 h-full">
+        <div className="h-full flex items-center justify-center">
             {children}
         </div>
     )
