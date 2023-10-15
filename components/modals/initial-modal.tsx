0e2f0a51513bcb8c95b5b920868f38a12dea5966
Nika <nikabilanych@gmail.com>
2023-10-15 17:54:43 +0200

    started initial modal - zod & react hook

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,10 +1,54 @@
 "use client"
 
-export const InitialModal = () => {
-    return (
+import { Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle}  
+    from "@/components/ui/dialog"
+    
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
 
-        <div>
-        This will be a modal!
-    </div>
-    )
-}
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Server name is required."
+    }),
+    imageUrl: z.string().min(1,{
+        message: "Server image is required."
+}),
+})
+export const InitialModal = () => {
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            imageUrl: ""
+    }
+});
+    //disable input if already submiting a request
+    const isLoading = form.formState.isSubmitting;
+    const onSubmit
+    const i
+    
+    return (
+        <Dialog open>
+            <DialogContent
+            className="bg-white text-black p-1
+            overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                        Customise your server
+                    </DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                    Give your server a personality by giving it a name
+                    </DialogDescription>
+                </DialogHeader>
+                <form></form>
+            </DialogContent>
+        </Dialog>
+    );
+};
+
